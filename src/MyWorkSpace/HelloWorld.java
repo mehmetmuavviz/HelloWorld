577df6752aff952f5fa079a09356e2236cedb2e3
mehmetmuavviz <muavvizmir@gmail.com>
2023-12-14 13:01:06 +0300

    add .

---
+++ src/MyWorkSpace/HelloWorld.java
@@ -0,0 +1,13 @@
+package MyWorkSpace;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        System.out.println("Hallo Welt");
+
+        System.out.println("Merhaba Dünya");
+
+    }
+}
